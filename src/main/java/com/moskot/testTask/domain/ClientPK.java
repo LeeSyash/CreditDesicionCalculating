c2da2da6d6eefc181ea9849126388f63a21c66e0
Москот Олександр Олександрович <aleksandr.moskot@privatbank.ua>
2022-08-22 18:26:30 +0300

    Updated PK classes

--- src/main/java/com/moskot/testTask/domain/ClientPK.java
+++ src/main/java/com/moskot/testTask/domain/ClientPK.java
@@ -2,15 +2,29 @@ package com.moskot.testTask.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.IdClass;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-@Getter @Setter
+@Getter @Setter @ToString
 @IdClass(ClientPK.class)
 public class ClientPK implements Serializable {
     private Integer idClient;
     private Date dateCurr;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientPK clientPK = (ClientPK) o;
+        return idClient.equals(clientPK.idClient) && dateCurr.equals(clientPK.dateCurr);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idClient, dateCurr);
+    }
 }
--- src/main/java/com/moskot/testTask/domain/CreditPK.java
+++ src/main/java/com/moskot/testTask/domain/CreditPK.java
@@ -2,13 +2,28 @@ package com.moskot.testTask.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.IdClass;
 import java.io.Serializable;
+import java.util.Objects;
 
-@Getter @Setter
+@Getter @Setter @ToString
 @IdClass(CreditPK.class)
 public class CreditPK implements Serializable {
     private Integer idClient;
     private Integer idCredit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreditPK creditPK = (CreditPK) o;
+        return idClient.equals(creditPK.idClient) && idCredit.equals(creditPK.idCredit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idClient, idCredit);
+    }
 }
